cc66e66f567eea25614f8a546993eaef6df4787b
zipper97412 <zipper97412@gmail.com>
2015-09-10 17:29:26 +0200

    Create sleep.c

---
+++ sleep.c
@@ -0,0 +1,52 @@
+#include "sleep.h"
+#include "stm8s.h"
+
+#define VIT1 12
+#define VIT2 7
+#define VIT3 6
+#define VIT4 5
+
+void elem_sleep(void) {
+	TIM4->CNTR=0x00;
+	TIM4->CR1=(TIM4->CR1 | TIM4_CR1_CEN);
+	while ((TIM4->SR1 & TIM4_SR1_UIF) != TIM4_SR1_UIF);
+	TIM4->SR1=~(TIM4_SR1_UIF) & TIM4->SR1;
+	TIM4->CR1=(TIM4->CR1 & ~TIM4_CR1_CEN);
+}
+
+void sleep(int potar) 
+{
+	int i;
+	switch(potar) 
+	{
+		case 1: 
+			for(i=0;i<VIT1;i++) 
+			{
+				elem_sleep();
+			}
+		break;
+		case 2: 
+			for(i=0;i<VIT2;i++) 
+			{
+				elem_sleep();
+			}
+		break;
+		case 3: 
+			for(i=0;i<VIT3;i++) 
+			{
+				elem_sleep();
+			}
+		break;
+		case 4: 
+			for(i=0;i<VIT4;i++) 
+			{
+				elem_sleep();
+			}
+		break;
+		default:
+			for(i=0;i<potar;i++)
+			{
+				elem_sleep();
+			}
+	}
+}
