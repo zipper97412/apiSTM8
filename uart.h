0a78575688c282f1902f230443e073aeaf6ffb99
zipper97412 <zipper97412@gmail.com>
2015-09-10 11:20:14 +0200

    Update uart.h

--- uart.h
+++ uart.h
@@ -1,17 +1,15 @@
-/* UART */
-
 
 /**
 \brief cette fonction permet d'initialiser l'UART
 */
-void UART_init();
+void UART_init(void);
 
 
 /**
 \brief reÃ§oit un caractere par l'uart
 \return le caractÃ¨re reÃ§u, 0 sinon
 */
-char UART_getchar();
+char UART_getchar(void);
 
 
 /**
@@ -27,4 +25,8 @@ u8 UART_putchar(char car);
 \param chaine la chaine a envoyer
 \return 0 si tout s'est bien passe, 1 sinon
 */
-u8 UART_putstring(char* chaine);
+//u8 UART_putstring(char* chaine);
+u8 UART_putstring(char *chaine);
+
+
+#endif
